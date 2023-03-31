3cd1ec86f4020fb0f33d7fd7d99aa6c422d9fe4b
“nreupomckinsey” <naldo_reupo-musayon@mckinsey.com>
2023-03-30 20:36:30 -0500

    feat(API) update

--- API/Controllers/StudentController.cs
+++ API/Controllers/StudentController.cs
@@ -41,7 +41,6 @@ namespace API.Controllers
         {
             try
             {
-                
                 if (student.Name == "") // Error Client
                 {
                     return StatusCode(400);
